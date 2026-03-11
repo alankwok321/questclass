45177e8c853662119627fe30af66cb123c9a734e
alankwok321 <12077190+alankwok321@users.noreply.github.com>
2026-03-11 07:06:11 +0000

    Remove unused frontend parts and simplify UI

--- public/js/firebase-config.example.js
+++
@@ -1,8 +0,0 @@
-window.QUESTCLASS_FIREBASE_CONFIG = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
